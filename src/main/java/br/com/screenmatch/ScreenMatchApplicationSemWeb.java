c042133fe40b0c4122fe803f89c94b88aa29b88e
RafaelPierotti <rafaelpierotti236@gmail.com>
2025-01-05 20:36:34 -0300

    Conectando a Rest

---
+++ src/main/java/br/com/screenmatch/ScreenMatchApplicationSemWeb.java
@@ -0,0 +1,25 @@
+package br.com.screenmatch;
+
+import br.com.screenmatch.principal.Principal;
+import br.com.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class ScreenMatchApplicationSemWeb implements CommandLineRunner {
+//	@Autowired
+//	private SerieRepository repositorio;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenMatchApplicationSemWeb.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repositorio);
+//		principal.exibeMenu();
+//
+//	}
+//}
---
+++ src/main/java/br/com/screenmatch/service/SerieService.java
@@ -0,0 +1,45 @@
+package br.com.screenmatch.service;
+
+import br.com.screenmatch.dto.SerieDTO;
+import br.com.screenmatch.model.Serie;
+import br.com.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository serieRepository;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return converteDados(serieRepository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series(){
+        return converteDados(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series.stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(serieRepository.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = serieRepository.findById(id);
+
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
+    }
+}
